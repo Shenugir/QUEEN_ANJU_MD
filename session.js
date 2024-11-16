594e39dd55c9f37826536cb26a8d02d6fc680d87
Shenugir <senuqueen075@gmail.com>
2024-11-16 09:12:14 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=7UFFQJyK#-9AOAky2u_nvx6C8tXDWuRIzzcPfDNNjxCtgNbKL9Uo",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=6QU3UZpS#pFR188gMG_FQUoWyGemWiZJCfEBelUI2YMLikf47iio",
 };
